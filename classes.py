aa6130a19756889c9a60e90ec539c18927128c18
HongyingT <110515428+HongyingT@users.noreply.github.com>
2022-08-11 22:54:57 -0500

    Add files via upload
    Revised unit tests for all the methods in the Television class

--- classes.py
+++ classes.py
@@ -3,18 +3,15 @@ class Television:
     A class representing details for a television object
     """
 
-    MIN_CHANNEL: int = 0  # Minimum TV channel
-    MAX_CHANNEL: int = 3  # Maximum TV channel
+    MIN_CHANNEL = 0  # Minimum TV channel
+    MAX_CHANNEL = 3  # Maximum TV channel
 
-    MIN_VOLUME: int = 0  # Minimum TV volume
-    MAX_VOLUME: int = 2  # Maximum TV volume
+    MIN_VOLUME = 0  # Minimum TV volume
+    MAX_VOLUME = 2  # Maximum TV volume
 
     def __init__(self) -> None:
         """
         Constructor to create initial state of a television object.
-        param channel: the TV channel. It should be set to the minimum TV channel by default.
-        param volume: the TV volume. It should be set to the minimum TV volume by default.
-        param power: the TV power. The TV should start when it is off.
         """
         self.__channel = Television.MIN_CHANNEL
         self.__volume = Television.MIN_VOLUME
@@ -23,8 +20,6 @@ class Television:
     def power(self) -> None:
         """
         Method to access the TV power.
-        :return: - If called on a TV object that is off, the TV object should be turned on.
-                 - If called on a TV object that is on, the TV object should be turned off.
         """
 
         if not self.__power:
@@ -35,9 +30,6 @@ class Television:
     def channel_up(self) -> None:
         """
         Method should be used to adjust the TV channel by incrementing its value.
-        :return:
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MAX_CHANNEL, it should take the TV channel back to the MIN_CHANNEL.
         """
         if self.__power:
             if self.__channel == Television.MAX_CHANNEL:
@@ -48,9 +40,6 @@ class Television:
     def channel_down(self) -> None:
         """
         Method should be used to adjust the TV channel by decrementing its value.
-        :return:
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MIN_CHANNEL, it should take the TV channel back to the MAX_CHANNEL.
         """
         if self.__power:
             if self.__channel == Television.MIN_CHANNEL:
@@ -61,9 +50,6 @@ class Television:
     def volume_up(self) -> None:
         """
         Method should be used to adjust the TV volume by incrementing its value.
-        :return:
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MAX_VOLUME, the volume should not be adjusted.
         """
         if self.__power:
             if self.__volume == Television.MAX_VOLUME:
@@ -74,9 +60,6 @@ class Television:
     def volume_down(self) -> None:
         """
         Method should be used to adjust the TV volume by decrementing its value.
-        :return:
-        - It should only work for a TV that is on.
-        - If the method is called when one is on the MIN_VOLUME, the volume should not be adjusted.
         """
         if self.__power:
             if self.__volume == Television.MIN_VOLUME:
